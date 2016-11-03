91f5c4623045cba5af6f49d9fdd81220e73529ce
Luisa Sepulveda <lsepulveda@endava.com>
2016-11-03 13:49:04 -0500

    First version BiggerIsGreater

---
+++ Algorithms/src/com/luisa/algorithms/BiggerIsGreater.java
@@ -0,0 +1,51 @@
+package com.luisa.algorithms;
+
+public class BiggerIsGreater {
+
+	public static void main(String[] args) {
+		String word = "dkhc";
+		char[] array = word.toCharArray();
+		nextPermutation(array);
+	}
+
+	public static void nextPermutation(char[] array) {
+		// Find longest non-increasing suffix
+		int i = array.length - 1;
+		while (i > 0 && array[i - 1] >= array[i])
+			i--;
+		// Now i is the head index of the suffix
+
+		// Are we at the last permutation already?
+		if (i <= 0) {
+			System.out.println("no answer");
+		} else {
+			// Let array[i - 1] be the pivot
+			// Find rightmost element that exceeds the pivot
+			int j = array.length - 1;
+			while (array[j] <= array[i - 1])
+				j--;
+			// Now the value array[j] will become the new pivot
+			// Assertion: j >= i
+
+			// Swap the pivot with j
+			char temp = array[i - 1];
+			array[i - 1] = array[j];
+			array[j] = temp;
+
+			// Reverse the suffix
+			j = array.length - 1;
+			while (i < j) {
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				i++;
+				j--;
+			}
+			StringBuilder result = new StringBuilder();
+			for (int s = 0; s < array.length; s++) {
+				result.append(array[s]);
+			}
+			System.out.println(result);
+		}
+	}
+}
